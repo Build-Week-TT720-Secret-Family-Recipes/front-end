859add30d94fed2fd5349a83a603d1e332f6c589
Emilio <3357168+Dellrodar@users.noreply.github.com>
2020-09-17 15:10:30 -0400

    Resolved Setup (#3)
    * Resolved issue with node_modules folder
    * All branches will need fresh pull from  main after merge.

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -1,11 +1,59 @@
 import React from 'react';
 
+//STYLING
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import {useStyles} from '../styles/styles'
+
+//CRUD
+//import axios from "axios";
+
 function LogIn(props) {
+      const classes = useStyles();
     return (
-        <div>
-            
-        </div>
-    );
+            <Grid container className={classes.loginFormContainer}>
+                <Paper className={classes.loginFormPaper}>
+                    <Grid item className={classes.loginFormItem}>
+                        <Typography variant="h2" className={classes.loginHeader}>
+                            Log In
+                        </Typography>
+                        <form onSubmit={null} className={classes.loginForm}>
+                            <label>
+                                <input
+                                    placeholder="Username"
+                                    type="text"
+                                    onChange={null}
+                                    name="username"
+                                    value={null}
+                                    errors={null}
+                                />
+                            </label>
+                            <label>
+                                <input
+                                    placeholder="Password"
+                                    type="text"
+                                    onChange={null}
+                                    name="password"
+                                    value={null}
+                                    errors={null}
+                                />
+                            </label>
+                        </form>
+                    </Grid>
+                    <Grid item className={classes.loginButtonItem}>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            style={{ color: "white" }}
+                            onClick={null}
+                            className={classes.loginButton}
+                        >LOG IN</Button>
+                    </Grid>
+                </Paper>
+            </Grid>
+        );
 }
 
 export default LogIn;
