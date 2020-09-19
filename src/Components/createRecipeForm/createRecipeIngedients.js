88c4fc6789c6b0fce4a0944f5cfb7b204beba60c
Sean Kramer <seandominickramer@gmail.com>
2020-09-18 22:01:53 -0400

    Correct paths to styles.js in createRecipeForm

--- src/Components/createRecipeForm/createRecipeIngedients.js
+++ src/Components/createRecipeForm/createRecipeIngedients.js
@@ -7,7 +7,7 @@ import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
-import { useStyles } from "/Users/whitebear/Sean/Lambda/Lambda_assignments/unit3/front-end/src/Styles/Styles.js";
+import { useStyles } from "../../Styles/Styles";
 import TextField from '@material-ui/core/TextField'
 
 //CRUD
--- src/Components/createRecipeForm/createRecipeInstructions.js
+++ src/Components/createRecipeForm/createRecipeInstructions.js
@@ -10,7 +10,7 @@ import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
-import { useStyles } from "/Users/whitebear/Sean/Lambda/Lambda_assignments/unit3/front-end/src/Styles/Styles.js";
+import { useStyles } from "../../Styles/Styles";
 import TextField from "@material-ui/core/TextField";
 import { gsap } from "gsap";
 //CRUD
