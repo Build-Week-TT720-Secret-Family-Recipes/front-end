d02236881039c0067f1db62da3ba6ff400269f4c
Sean Kramer <seandominickramer@gmail.com>
2020-09-20 15:40:39 -0400

    Add reducer and correct paths to styles

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import { Provider } from "react-redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import { applyMiddleware, compose, createStore } from "redux";
+import rootReducer from './Reducer';
+
 import App from './Components/App';
 
 
 
 const enhancer = compose(applyMiddleware(thunk, logger));
 
-const store = createStore(enhancer);
+const store = createStore(rootReducer, enhancer);
 
 
 ReactDOM.render(
